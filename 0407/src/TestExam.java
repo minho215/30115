5d41dab025113ba909c748cc46e773dfe9f864a4
jmh <jmh@gmail.com>
2023-04-07 10:08:22 +0900

    0407 프로젝트 추가

---
+++ 0407/src/TestExam.java
@@ -0,0 +1,18 @@
+
+public class TestExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum=0, i = 1;
+		
+			sum += i;
+			if (i % 3 ==0 && i % 7 ==0) {
+			System.out.println("3의 배수이면서 7의 배수 합  = " + sum + "입니다");
+			
+			}
+			i++;
+			
+			while(i<=500);
+	}
+
+}
